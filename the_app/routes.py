92604a9df31bc6825b190dba4df5d553806d59a7
Mon Gomez <nulo76@yahoo.es>
2020-06-21 11:43:07 +0100

    media tabla status

--- the_app/routes.py
+++ the_app/routes.py
@@ -60,7 +60,31 @@ def operar():
 
 @app.route('/status')
 def status():
-    return 'Aqui va la pagina de estado de las inversiones'
+    conn= sqlite3.connect(app.config['BBDD'])
+    c= conn.cursor()
+
+    query= "SELECT id, criptomoneda, volumen, importe, operacion, cotizacion FROM resumen;"
+    operaciones= c.execute(query).fetchall()
+    
+    conn.close()
+    
+    if operaciones:
+    
+        d={}
+        k=d.keys()
+        for fila in operaciones:
+            if (fila[1]) in k:
+                if fila[4]=='Compra':
+                    d[fila[1]]['volumen'] += (fila[2])
+                    d[fila[1]]['importe'] += (fila[3])
+                else:
+                    d[fila[1]]['volumen'] -= (fila[2])
+                    d[fila[1]]['importe'] -= (fila[3])
+            else:
+                d[fila[1]]= {'volumen': (fila[2]), 'importe':(fila[3]), 'cotizacion': (fila[5])}
+
+
+    return render_template('status.html', ops= d)
 
 
 
