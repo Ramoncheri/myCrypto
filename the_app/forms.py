8a023e9c949edc4b571acf39f8a7c380cd9b1175
Mon Gomez <nulo76@yahoo.es>
2020-06-30 16:25:44 +0100

    app funcionando con todas las monedas

--- the_app/forms.py
+++ the_app/forms.py
@@ -1,10 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, SubmitField, SelectField, DateTimeField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import DataRequired, EqualTo, ValidationError
+
+
+def valida_posibilidad_compra(form, field):
+    if field.data ==form.Moneda_from.data:
+        raise ValidationError("No es posible realizar ningún intercambio entre monedas iguales")
+    elif field.data != "BTC" and form.Moneda_from.data == "EUR": 
+        raise ValidationError("No es posible intercambiar EUR, por {} directamente. Sólo puede adquirir {}, con otras criptomonedas.".format(field.data,field.data ))
+    elif field.data == "EUR" and form.Moneda_from.data != "BTC":
+        raise ValidationError("No es posible cambiar {} por EUR, directamente. Sólo es posible intercambiar BTC por EUR. Si desea EUR, por favor, intercambie  antes sus {} a BTC y vuelva a intentarlo.".format(form.Moneda_from.data, form.Moneda_from.data))
+
+
+
+criptos = ("","EUR", "BTC", "ETH", "XRP", "LTC", "BCH", "BNB", "USDT", "EOS", "BSV", "XLM", "ADA", "TRX")
 
 class CompraForm(FlaskForm):
-    criptoFrom= SelectField(u'From ', choices= [(''), ('EUR'), ('BTC'), ('ETH'), ('LTC')], validators=[EqualTo('criptoTo', message="No puede ser igual a la moneda comprada")])
-    criptoTo= SelectField(u'To ', choices=[(''), ('EUR'), ('BTC'), ('ETH'), ('LTC')])
+    criptoFrom= SelectField(u'From ', choices= [(cripto) for cripto in criptos], validators=[EqualTo('criptoTo', message="No puede ser igual a la moneda comprada")])
+    criptoTo= SelectField(u'To ', choices=[(cripto) for cripto in criptos], validators=[valida_posibilidad_compra])
     cantidadFrom= FloatField('Cantidad', validators=[DataRequired(message='Campo requerido')])
     calcular= SubmitField('Calcular')
 
@@ -17,4 +30,7 @@ class CompraForm(FlaskForm):
     #cotizacion= FloatField('Cotización')
 
     
+      
+
+
     
--- the_app/routes.py
+++ the_app/routes.py
@@ -9,12 +9,11 @@ from the_app.functions import calc_fecha
 
 @app.route('/')
 def index():
-    #price= api.convert()
 
     conn= sqlite3.connect(app.config['BBDD'])
     c= conn.cursor()
 
-    query= "SELECT id, de_cripto, volumen,importe, operacion FROM resumen;"
+    query= "SELECT id, de_cripto, volumen,importe, a_cripto FROM resumen;"
     operaciones= c.execute(query).fetchall()
     
     conn.close()
@@ -24,22 +23,25 @@ def index():
         d={}
         k=d.keys()
         for fila in operaciones:
-            if (fila[1]) in k:
-                if fila[4]=='Compra':
-                    d[fila[1]]['volumen'] += (fila[2])
-                    
-                else:
-                    d[fila[1]]['volumen'] -= (fila[2])
-                    
+            if fila[4] not in k:
+                d[fila[4]]= {'volumen': (fila[3])}
+                if fila[1] not in k:
+                    d[fila[1]]={'volumen': -(fila[2])}
+                else: 
+                    d[fila[1]]['volumen'] -= fila[2]
+                
             else:
-                d[fila[1]]= {'volumen': (fila[2])} 
+                d[fila[4]]['volumen'] += (fila[3])
+                if fila[1] not in k:
+                    d[fila[1]]={'volumen': -(fila[2])}
+                else: 
+                    d[fila[1]]['volumen'] -= fila[2]
+                
             
-        
-        
         for symbol in k:
             symbol_from=symbol
-            price= api.convert(symbol_from)
-            d[symbol_from]['importe']= price
+            cotiz= api.convert(symbol_from)
+            d[symbol_from]['importe']= cotiz
         
         return render_template('inicio.html', ops= d)
 
@@ -47,22 +49,23 @@ def index():
         return render_template('sin_movimientos.html')
 
 
-@app.route('/detalle', methods=["GET"])
+@app.route('/detalle')
 
 def detalle_ops():
-    if request.method== 'GET':
-        cryptoM= request.values['id']
-        conn=sqlite3.connect(app.config['BBDD'])
-        c= conn.cursor()
-        
-        query= "SELECT fecha, hora, operacion, volumen, cotizacion, importe, de_cripto FROM resumen WHERE de_cripto=?"
-        op_crypto= c.execute(query, (cryptoM,)).fetchall()
-        
-        conn.close()
+    
+    cryptoM= request.values['id']
+    conn=sqlite3.connect(app.config['BBDD'])
+    c= conn.cursor()
+    
+    query= "SELECT fecha, volumen, cotizacion, importe, a_cripto, de_cripto FROM resumen WHERE a_cripto=? or de_cripto=?"
+    datos=((cryptoM), (cryptoM))
+    op_crypto= c.execute(query,datos).fetchall()
+    
+    conn.close()
     
 
     
-    return render_template('detalle.html', titulo= 'My Crypto', op_crypto= op_crypto, cryptoM=cryptoM)
+    return render_template('detalle.html', op_crypto= op_crypto, cryptoM=cryptoM)
 
 
 @app.route('/operar', methods=["GET", "POST"])
@@ -72,8 +75,11 @@ def operar():
 
     if request.method=='GET':
         return render_template('formul_compra.html', form= form)
+
     else:
         if request.values.get('calcular'):
+            #comparar request.values.get(cantidadFrom) con la cantidad de cipto que tienes-->select
+            
             cantidadTo= api.convert(request.values.get('criptoFrom'), request.values.get('cantidadFrom'), request.values.get('criptoTo'))
             cotiz= cantidadTo/float(request.values.get('cantidadFrom'))
             fecha= calc_fecha()
@@ -82,16 +88,30 @@ def operar():
         elif request.values.get('cancelar'):
             
             return redirect(url_for("operar"))
+
         else:
-            return 'se va por aqui'
-        
+
+            conn=sqlite3.connect(app.config['BBDD'])
+            c= conn.cursor()
+            query= "INSERT INTO resumen(fecha, de_cripto, volumen, a_cripto, cotizacion, importe) values (?,?,?,?,?,?);"
+            datos= (request.values.get('fecha'), request.values.get('criptoFrom'), request.values.get('cantidadFrom'), request.values.get('criptoTo'), request.values.get('cotiz'), request.values.get('cantidadTo'))
+            c.execute(query, datos)
+
+            conn.commit()
+            conn.close()
+
+            return redirect(url_for('index'))
+
+
+
+
 
 @app.route('/status')
 def status():
     conn= sqlite3.connect(app.config['BBDD'])
     c= conn.cursor()
 
-    query= "SELECT id, de_cripto, volumen, importe, operacion, cotizacion FROM resumen;"
+    query= "SELECT id, de_cripto, volumen, importe, a_cripto FROM resumen;"
     operaciones= c.execute(query).fetchall()
     
     conn.close()
@@ -101,29 +121,35 @@ def status():
         d={}
         k=d.keys()
         for fila in operaciones:
-            if (fila[1]) in k:
-                if fila[4]=='Compra':
-                    d[fila[1]]['volumen'] += (fila[2])
-                    d[fila[1]]['importe'] += (fila[3])
-                else:
-                    d[fila[1]]['volumen'] -= (fila[2])
-                    d[fila[1]]['importe'] -= (fila[3])
+            if fila[4] not in k:
+                d[fila[4]]= {'volumen': (fila[3])}
+                if fila[1] not in k:
+                    d[fila[1]]={'volumen': -(fila[2])}
+                else: 
+                    d[fila[1]]['volumen'] -= fila[2]
+                
             else:
-                d[fila[1]]= {'volumen': (fila[2]), 'importe':(fila[3]), 'cotizacion': (fila[5])}
-
+                d[fila[4]]['volumen'] += (fila[3])
+                if fila[1] not in k:
+                    d[fila[1]]={'volumen': -(fila[2])}
+                else: 
+                    d[fila[1]]['volumen'] -= fila[2]
+            
+        
+        
         for symbol in k:
             symbol_from=symbol
-            price= api.convert(symbol_from)
-            d[symbol_from]['cot_actual']= price
-    
-    suma_total= 0
-    suma_total_act= 0
-    for fila in d.items():
-        suma_total += fila[1]['importe']
-        suma_total_act += fila[1]['volumen']*fila[1]['cot_actual']
+            cotiz= api.convert(symbol_from)
+            d[symbol_from]['importe']= cotiz
     
 
-    return render_template('status.html', ops= d, suma_total= suma_total, suma_total_act= suma_total_act)
+    suma_total_act= 0
+    for fila in d.items():
+        if fila[0] != 'EUR':
+            suma_total_act += fila[1]['volumen']*fila[1]['importe']
+    
+
+    return render_template('status.html', ops= d, suma_total_act= suma_total_act)
 
 
 
